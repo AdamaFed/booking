ae1d0291d14fb9e8cd008ce60f9159bdabebea26
Adama Saho <adamasaho@MacBook-Pro-von-Adama.local>
2024-06-09 02:29:38 +0200

    navbar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import {FontAwesomeModule, FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+import {ToastModule} from "primeng/toast";
+import {ToastService} from "./layout/toast.service";
+import {MessageService} from "primeng/api";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,12 +22,26 @@ import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 export class AppComponent implements OnInit{
 
   faIconLibrary = inject(FaIconLibrary)
+  isListingView:boolean =  true;
+  toastService = inject(ToastService);
+  messageService: MessageService = inject(MessageService);
 
   ngOnInit() {
     this.initFontAwesome();
+    this.listenToastService();
   }
 
   private initFontAwesome(){
     this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
+
+  private listenToastService(){
+    this.toastService.sendSub.subscribe({
+      next: newMessage  => {
+      if(newMessage && newMessage.summary !== this.toastService.INIT_STATE){
+        this.messageService.add(newMessage);
+      }
+    }
+    })
   }
 }
